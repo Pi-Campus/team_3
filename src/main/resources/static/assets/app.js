b851b1a9d0f77c21a8025af042bc3e6f2de868a3
Raffaele Sgarro <raffaelesgarro@gmail.com>
2021-03-15 21:26:43 +0100

    Thanks Detart!!!
    Typo

--- src/main/resources/static/assets/app.js
+++ src/main/resources/static/assets/app.js
@@ -3,25 +3,41 @@ $(function(){
         window.DATA = JSON.parse(window.JSON_DATA);
         // console.log(window.DATA);
         $("#json-raw").val(window.JSON_DATA);
+
+        const date = [];
+        datasets = [];
+
+        //create a new array of value1
+        window.DATA.data.map((e) => {
+            date.push(e.cells[0].value);
+        });
+
+        //create a new array of values2 and convert string to numbers
+        window.DATA.data.map((e) => {
+            datasets.push(parseFloat(e.cells[1].value.replace(",", ".")));
+        });
+
+        var ctx = document.getElementById("myChart").getContext("2d");
+        var chart = new Chart(ctx, {
+            // The type of chart we want to create
+            type: "line",
+
+            // The data for our dataset
+            data: {
+                labels: date,
+                datasets: [
+                    {
+                        label: "Covid-19 US Counties",
+                        backgroundColor: "#57a2ff",
+                        borderColor: "#57a2ff",
+                        data: datasets,
+                    },
+                ],
+            },
+
+            // Configuration options go here
+            options: {},
+        });
+
     }
-
-    const TheChart = echarts.init(document.getElementById('chart-1'));
-
-    TheChart.setOption({
-        tooltip: {},
-        xAxis: {
-            type: 'category',
-            data: window.DATA.data.map(p => p.cells[0].value)
-        },
-        yAxis: {},
-        series: [{
-            large: true,
-            name: 'Serie',
-            type: 'bar',
-            data: window.DATA.data.map(p => p.cells[1].value),
-            itemStyle: {
-                color: '#3699ff'
-            }
-        }]
-    });
 });
